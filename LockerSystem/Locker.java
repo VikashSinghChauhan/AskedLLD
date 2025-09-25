919ab36585483cea7d85de29ea56a363e3e21c8f
VikashSinghChauhan <85977687+VikashSinghChauhan@users.noreply.github.com>
2025-09-25 22:05:38 +0530

    Add files via upload

--- LockerSystem/Locker.java
+++ LockerSystem/Locker.java
@@ -1,5 +1,7 @@
 package com.example.SpringTest.LockerSystem;
 
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -12,6 +14,7 @@ public class Locker {
     LockerSize lockerSize;
     Package pkg;
     String passcode;
+    Long placingTime;
     List<Observer> observerList;
 
 
@@ -29,7 +32,9 @@ public class Locker {
         String passwd = String.valueOf((Math.random()*1000000)).substring(0,6);
         this.passcode = passwd;
         this.occupied=(true);
+        placingTime = Instant.now().toEpochMilli();
         observerList.add(ob);
+
 
         for(Observer obb : observerList)
         {
@@ -41,14 +46,18 @@ public class Locker {
     {
         if(occupied && passwd.equals(passcode))
         {
+            Long timeElasped = Instant.now().toEpochMilli() - placingTime;
+            timeElasped = timeElasped/(1000*60*60);
             Package temp = pkg;
             pkg = null;
             passcode = null;
             occupied = (false);
+            placingTime = null;
+
 
             for(Observer ob : observerList)
             {
-                ob.notify("Package released "+ temp.productId+" Thank you !");
+                ob.notify("Package released "+ temp.productId+" Thank you ! + time " + timeElasped + " hours");
             }
             observerList.clear();
             return temp;
